7d405a3e22a8c7bce4d99b0ac3c648f0005ae2f6
SenorGunter <163559186+SenorGunter@users.noreply.github.com>
2025-01-20 21:39:23 +0100

    api_fetch first commit

---
+++ api_fetch.py
@@ -0,0 +1,21 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+
+def get_stock_data(stock_id):
+	url = "https://seeking-alpha.p.rapidapi.com/symbols/get-profile"
+
+	querystring = {"symbols":stock_id}
+
+	headers = {
+		"x-rapidapi-key": api_key,
+		"x-rapidapi-host": "seeking-alpha.p.rapidapi.com"
+	}
+
+	response = requests.get(url, headers=headers, params=querystring)
+
+	return(response.json())
